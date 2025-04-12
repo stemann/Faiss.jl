cf29dd84b9fe3f8de0ccc5720ba9afab0dde5c8c
Jesper Stemann Andersen <jesper@sait.dk>
2025-04-12 09:07:17 +0200

    Debug CUDAExt

--- ext/CUDAExt/CUDAExt.jl
+++ ext/CUDAExt/CUDAExt.jl
@@ -2,6 +2,13 @@ module CUDAExt
 
 @info "Loading CUDAExt.jl"
 
+foo() = "bar"
+
 include(joinpath(@__DIR__, "Wrapper.jl"))
+
+get_num_gpus() = let num_gpus = Ref{Cint}(-1)
+    Wrapper.faiss_get_num_gpus(num_gpus)
+    num_gpus[]
+end
 
 end
